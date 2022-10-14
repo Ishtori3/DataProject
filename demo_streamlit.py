4d7bc9b0fb9e2c1795f4553e6856c17ad279fd9b
Ishtori3 <115566517+Ishtori3@users.noreply.github.com>
2022-10-14 18:46:09 +0200

    Update demo_streamlit.py

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -105,7 +105,7 @@ elif page==pages[2]:
         #ax.set_ylabel("Puissance en MW")
         fig.set_size_inches(30, 10)
         
-        fig.suptitle("Prédictions pour 2022 en région " + str(region), weight="bold", fontsize=16)
+        plt.title("Prédictions pour 2022 en région " + str(region), weight="bold", fontsize=16)
        # ax.plot(df[df["Région"]==region].resample("D").mean().Consommation["2022"], color="red", label="Données réelles", ax=ax) 
         plt.legend(loc="upper right")
 
